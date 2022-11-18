523b2b4448ebacf7bed19fb5cb2ebc9e0c1c97de
Mischa Strauss <65661176+mimasala@users.noreply.github.com>
2022-11-18 20:06:09 +0100

    new task

--- src/main/java/ch/tbz/Main.java
+++ src/main/java/ch/tbz/Main.java
@@ -2,6 +2,6 @@ package ch.tbz;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World!3");
     }
 }
